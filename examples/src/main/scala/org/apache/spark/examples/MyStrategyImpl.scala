efff2b76ac17e0cebcf54c417f161db890946479
Chen Guo <chguo@linkedin.com>
2019-02-06 11:24:45 -0800

    Hook up a customized Join Strategy

--- examples/src/main/scala/org/apache/spark/examples/MyStrategyImpl.scala
+++ examples/src/main/scala/org/apache/spark/examples/MyStrategyImpl.scala
@@ -2,7 +2,11 @@
 // scalastyle:off println
 package org.apache.spark.examples
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions.{Alias, EqualTo}
+import org.apache.spark.sql.catalyst.plans.Inner
+import org.apache.spark.sql.catalyst.plans.logical.{Join, LogicalPlan, Range}
+import org.apache.spark.sql.execution.{ProjectExec, RangeExec, SparkPlan}
+import org.apache.spark.sql.{SparkSession, Strategy}
 
 object MyStrategyImpl {
   def main(args: Array[String]) {
@@ -14,6 +18,8 @@ object MyStrategyImpl {
       .getOrCreate()
 
     val sc = spark.sparkContext
+
+    spark.sqlContext.experimental.extraStrategies = IntervalJoin :: Nil
 
     // Set the broadcast join threshold to be 1MB
     spark.sql("SET spark.sql.autoBroadcastJoinThreshold=1048576")
@@ -28,6 +34,31 @@ object MyStrategyImpl {
     result.explain
 
     spark.stop()
+  }
+}
+
+case object IntervalJoin extends Strategy with Serializable {
+  override def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
+    case Join(
+    Range(start1, end1, 1, part1, Seq(o1), isStreaming1),
+    Range(start2, end2, 1, part2, Seq(o2), isStreaming2),
+    Inner,
+    Some(EqualTo(e1, e2)))
+      if ((o1 semanticEquals e1) && (o2 semanticEquals e2)) ||
+        ((o1 semanticEquals e2) && (o2 semanticEquals e1)) => {
+      if ((start1 <= end2) && end1 >= end2) {
+        val start = math.max(start1, start2)
+        val end = math.min(end1, end2)
+        val part = math.max(part1.getOrElse(200), part2.getOrElse(200))
+        val result = RangeExec(Range(start, end, 1, Some(part), o1 :: Nil, false))
+        val twoColumns = ProjectExec(Alias(o1, o1.name)(exprId = o1.exprId) :: Nil, result)
+        twoColumns :: Nil
+      }
+      else {
+        Nil
+      }
+    }
+    case _ => Nil
   }
 }
 
